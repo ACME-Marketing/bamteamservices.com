aec6adeac380929f74c45ea74c674cdb6611e0e1
saltlifeshep <62799544+saltlifeshep@users.noreply.github.com>
2025-07-01 15:48:28 -0400

    feat: customize Tailwind color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   "content": [
-    "./src/**/*.{astro,html,js,jsx,ts,tsx,vue}",
-    "./node_modules/astro/.temp/**/*"
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"
   ],
   "theme": {
-    "extend": {}
+    "extend": {
+      "colors": {
+        "primary": "#003366",
+        "secondary": "#4472C4",
+        "accent": "#FFC000",
+        "background": "#F3F3F3",
+        "text": "#333333",
+        "success": "#548235",
+        "warning": "#FF0000"
+      },
+      "fontFamily": {
+        "sans": [
+          "Inter",
+          "system-ui",
+          "sans-serif"
+        ]
+      }
+    }
   },
   "plugins": [
     "@tailwindcss/typography"
