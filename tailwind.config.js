04ce6b14d856cf03860f5a9fe4167fabd02c69a7
saltlifeshep <62799544+saltlifeshep@users.noreply.github.com>
2025-07-01 16:18:30 -0400

    chore: rebuild with new theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,11 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  "content": [
-    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"
+  content: [
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
-  "theme": {
-    "extend": {
-      "colors": {
-        "primary": "#003366",
-        "secondary": "#4472C4",
-        "accent": "#FFC000",
-        "background": "#F3F3F3",
-        "text": "#333333",
-        "success": "#548235",
-        "warning": "#FF0000"
-      },
-      "fontFamily": {
-        "sans": [
-          "Inter",
-          "system-ui",
-          "sans-serif"
-        ]
-      }
-    }
+  theme: {
+    extend: {},
   },
-  "plugins": [
-    "@tailwindcss/typography"
-  ]
-}
+  plugins: [require('@tailwindcss/typography')],
+};
